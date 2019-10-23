e2a20aca085a0fbbb9f09eef3ee4c2ab4513113e
fv3172 <55358376+fv3172@users.noreply.github.com>
2019-10-23 15:46:50 -0400

    Update mainForLinklist.cpp

--- mainForLinklist.cpp
+++ mainForLinklist.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 #include "String"
 #include "list.h"
-
-
+#include <fstream>
 
 /* Driver code*/
 int main()
@@ -35,6 +34,7 @@ int main()
 		cout << "Enter 1 to insert a url \n"
 			"Enter 2 to delete a url \n"
 			"Enetr 3 to print all url \n"
+			"Enetr 4 to see which fle is converting \n"
 			"Enter 0 to exit \n"
 			"Enter a selection --> ";
 		cin >> selection;
@@ -48,6 +48,7 @@ int main()
 		switch (selection) {
 		case 1:
 			cout << "get the url --> ";
+			//getline(cin, url);	
 			cin >> url;
 			cout << endl;
 			modifyList.insert(&head, url);
@@ -57,18 +58,21 @@ int main()
 			cin >> url;
 			modifyList.deleteItem(&head, url);
 			break;
-			case 3
-				:
-					modifyList.printList(head);
-					break;
-
-			default:
-				cout << "invalid input \n";
+		case 3:
+			modifyList.printList(head);
+			break;
+		case 4:
+			modifyList.Convirting(head);
+			break;
+		default:
+			cout << "invalid input \n";
 		}
+
 
 		cout << "Enter 1 to insert a url \n"
 			"Enter 2 to delete a url \n"
 			"Enetr 3 to print all url \n"
+			"Enetr 4 to see which fle is converting \n"
 			"Enter 0 to exit \n"
 			"Enter a selection --> ";
 		cin >> selection;
@@ -83,6 +87,7 @@ int main()
 			cout << "Enter 1 to insert a url \n"
 				"Enter 2 to delete a url \n"
 				"Enetr 3 to print all url \n"
+				"Enetr 4 to see which fle is converting \n"
 				"Enter 0 to exit \n"
 				"Enter a selection --> ";
 			cin >> selection;
@@ -90,6 +95,7 @@ int main()
 		}
 	}
 
+	modifyList.processConvirting(head);
 
 
 	// Show application close
