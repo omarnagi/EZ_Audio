d28f34fe817a53d23e143749af5676642540add7
fv3172 <55358376+fv3172@users.noreply.github.com>
2019-11-13 20:28:42 -0500

    Apply Abstract Factory Pattern
    This file is to modify the code according to the Abstract Factory Pattern

---
+++ GoogleDrive.py
@@ -0,0 +1,118 @@
+
+
+from __future__ import print_function
+import pickle
+import os.path
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+import sys
+from PyQt5.QtWidgets import (QWidget, QToolTip, QPushButton, QApplication)
+from PyQt5.QtGui import QFont
+
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+
+
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+
+    def initUI(self):
+        QToolTip.setFont(QFont('SansSerif', 10))
+
+        self.setToolTip('This is a <b>QWidget</b> widget')
+
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Tooltips')
+        btn.clicked.connect(self.authri)
+
+        if os.path.exists('token.pickle'):
+            btn.clicked.connect(self.on_click)
+        else:
+            btn.clicked.connect(self.of_click)
+
+        self.show()
+
+
+
+
+    @pyqtSlot()
+    def authri(self):
+
+        """Shows basic usage of the Drive v3 API.
+        Prints the names and ids of the first 10 files the user has access to.
+        """
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+
+
+
+
+
+
+
+        service = build('drive', 'v3', credentials=creds)
+
+        # # Call the Drive v3 API
+        # results = service.files().list(
+        #     pageSize=10, fields="nextPageToken, files(id, name)").execute()
+        # items = results.get('files', [])
+        #
+        # if not items:
+        #     print('No files found.')
+        # else:
+        #     print('Files:')
+        #     for item in items:
+        #         print(u'{0} ({1})'.format(item['name'], item['id']))
+
+    @pyqtSlot()
+    def on_click(self):
+        textboxValue = "The authri ws secssefual "
+        QMessageBox.question(self,  'Message - pythonspot.com', "You typed: " + textboxValue,  QMessageBox.Ok, QMessageBox.Ok)
+        self.textbox.setText("")
+
+    @pyqtSlot()
+    def of_click(self):
+        textboxValue = "nooooooo "
+        QMessageBox.question(self,  'Message - pythonspot.com', "You typed: " + textboxValue,  QMessageBox.Ok, QMessageBox.Ok)
+        self.textbox.setText("")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
