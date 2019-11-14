49350987c4baa064173460eb65f60319d27eff1c
omarnagi <55221949+omarnagi@users.noreply.github.com>
2019-11-13 20:44:39 -0500

    Add files via upload

---
+++ Validate URL.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	//https://www.youtube.com/watch?v=WK0YhfKqdaI&feature=youtu.be
+	string url;
+	string host;
+	
+		cout << "Please enter the URL" << endl;
+		cin >> url;
+
+		size_t found = url.find_first_of("//");
+		host = url.substr(found + 2, 15);
+
+	 while (host != "www.youtube.com")
+	{
+		cout << "the URL is NOT a YouTube URL  " << endl;
+		cout << "Please enter a new URL " << endl;
+		cin >> url;
+		size_t found = url.find_first_of("//");
+		host = url.substr(found + 2, 15);
+	}
+	cout << "the URL is a YouTube URL  " << endl;
+	
+	
+	
+	
+
+
+	return 0;
+}
